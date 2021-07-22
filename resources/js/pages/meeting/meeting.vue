4a07d49dcb398c1117f9cc9ba88eb612ddbd2054
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-07-22 21:21:13 +0900

    check custom URI scheme
    implemented to check custom URI scheme

--- resources/js/pages/meeting/meeting.vue
+++ resources/js/pages/meeting/meeting.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="row">
     <div class="col-lg-8 m-auto">
-        <card :title="Meeting">
+        <card>
             <div class="form-group row">
                 <label class="col-md-4 col-form-label text-md-left">Meeting invitation link: </label>
                 <label class="col-md-4 col-form-label text-md-left">{{this.meetingLink}}</label>
@@ -12,8 +12,15 @@
 </div>
 </template>
 <script>
-import Button from '../../components/Button.vue'
+
+import ProtocolCheck from '../../misc/protocolcheck'
 export default {
+
+    components: {
+
+    },
+
+    middleware: 'guest',
 
     data() {
         return {
@@ -23,12 +30,30 @@ export default {
     mounted() {
         this.meetingLink =  Math.floor(Math.random() * 1000000).toString();
     },
+    metaInfo () {
+        return { title: this.$t('meeting') }
+    },
     methods: {
         onStartMeeting: function(event) {
-            
-            // HERE 
-
-        }
+            ProtocolCheck("vrmba://" + this.meetingLink,
+                function () {
+                    var retVal = confirm("You didn't install VRMBA Test App. Are you going to download and install it?");
+                    if( retVal == true ) {
+                        window.location.href = "/dist/UE4Setup.msi";
+                    } 
+                }
+            );
+        },
+        downloadFile() {
+            this.$http.get(this.appApiPath + '/testpdf', {responseType: 'arraybuffer'})
+            .then(response => {
+                let blob = new Blob([response.data], { type: 'application/pdf' })
+                let link = document.createElement('a')
+                link.href = window.URL.createObjectURL(blob)
+                link.download = 'test.pdf'
+                link.click()
+            })
+        }        
     }
     
 }
