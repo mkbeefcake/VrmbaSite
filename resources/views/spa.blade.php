b2c91c7e652fa70b784b567009ce4d2407d0c77d
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-07-30 01:29:05 +0900

    integrate black.css

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -15,6 +15,14 @@ $config = [
   <title>{{ config('app.name') }}</title>
 
   <link rel="stylesheet" href="{{ mix('dist/css/app.css') }}">
+  <!-- Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:200,300,400,600,700,800" rel="stylesheet" />
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+  <!-- Icons -->
+  <link href="{{ asset('black') }}/css/nucleo-icons.css" rel="stylesheet" />
+  <!-- CSS -->
+  <link href="{{ asset('black') }}/css/black-dashboard.css?v=1.0.0" rel="stylesheet" />
+  <link href="{{ asset('black') }}/css/theme.css" rel="stylesheet" />
 </head>
 <body>
   <div id="app"></div>
